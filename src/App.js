bf3c23ead561fbe5a8efec4d8b7ee24f68fac8c8
FernandoG8 <al064504@uacam.mx>
2025-05-13 19:07:00 -0600

    Rama Combinada sin el error de las rutas

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import SobreNosotros from './pages/SobreNosotros';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/checkout/Layout';
 import { AuthProvider } from './context/AuthContext';
+
 const App = () => {
   return (
     <AuthProvider>
@@ -40,55 +41,16 @@ const App = () => {
         <Route path="/Registro" element={<RegistroUsuarios />} />
 
 
-        {/* ============= RUTAS PROTEGIDAS DE LA TIENDA (CON LAYOUT Y PADDING) ============= */}
-        <Route path="/" element={
-          <ProtectedRoute requiredRole={['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SELLER']}>
-            <Layout withNavbarPadding={true}>
-              <StoreHome />
-            </Layout>
-          </ProtectedRoute>
-        } />
+        {/* ============= RUTAS PÚBLICAS DE LA TIENDA (CON LAYOUT Y PADDING) ============= */}
+        <Route path="/" element={<Layout withNavbarPadding={true} ><StoreHome /></Layout>} />
+        <Route path="/tienda" element={<Layout withNavbarPadding={false}><StoreHome /></Layout>} />
+        <Route path="/menu" element={<Layout withNavbarPadding={true} navbarSolid={true}><Menu /></Layout>} />
+        <Route path="/about" element={<Layout withNavbarPadding={true} navbarSolid={true}><SobreNosotros /></Layout>} />
+        <Route path="/checkout" element={<Layout withNavbarPadding={true} navbarSolid={true}><Checkout /></Layout>} />
+        <Route path="/pedidosUsuario" element={<Layout withNavbarPadding={true} navbarSolid={true}><PedidosUsuario /></Layout>} />
 
-        <Route path="/tienda" element={
-          <ProtectedRoute requiredRole={['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SELLER']}>
-            <Layout withNavbarPadding={false}>
-              <StoreHome />
-            </Layout>
-          </ProtectedRoute>
-        } />
-
-        <Route path="/menu" element={
-          <ProtectedRoute requiredRole={['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SELLER']}>
-            <Layout withNavbarPadding={true} navbarSolid={true}>
-              <Menu />
-            </Layout>
-          </ProtectedRoute>
-        } />
-
-        <Route path="/checkout" element={
-          <ProtectedRoute requiredRole={['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SELLER']}>
-            <Layout withNavbarPadding={true} navbarSolid={true}>
-              <Checkout />
-            </Layout>
-          </ProtectedRoute>
-        } />
-
-  <Route path="/pedidosUsuario" element={<Layout withNavbarPadding={true} navbarSolid={true}><PedidosUsuario /></Layout>} />
-        {/* ============= PRODUCT DETAIL (PROTEGIDO) ============= */}
-        <Route path="/producto/:id" element={
-          <ProtectedRoute requiredRole={['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SELLER']}>
-            <Layout withNavbarPadding={false}>
-              <ProductDetail />
-            </Layout>
-          </ProtectedRoute>
-        } />
-
-        
-        <Route path="/about" element={
-          <Layout withNavbarPadding={true} navbarSolid={true}>
-            <SobreNosotros />
-          </Layout>
-        } />
+        {/* ============= PRODUCT DETAIL (SIN PADDING) ============= */}
+        <Route path="/producto/:id" element={<Layout withNavbarPadding={false}><ProductDetail /></Layout>} />
 
         {/* Rutas Admin agrupadas */}
         <Route
@@ -106,8 +68,11 @@ const App = () => {
           <Route path="pedidos" element={<AdminPedidos />} />
         </Route>
 
+        {/* ============= RUTAS PROTEGIDAS DE ADMINISTRADOR (SIN PADDING) ============= */}
+
+
         {/* ============= RUTA DE FALLBACK ============= */}
-        <Route path="*" element={<Navigate to="/login" replace />} /> {/* Cambiado a /login */}
+        <Route path="*" element={<Navigate to="/tienda" replace />} />
       </Routes>
     </AuthProvider>
   );
